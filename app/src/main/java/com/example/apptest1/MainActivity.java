9e63289462cc52ca55149bd0922eb6bf6b9b6c0c
Arjun Reddy <47860624+arjunkurukundu@users.noreply.github.com>
2020-01-22 15:12:31 -0600

    Tenth Commit

--- app/src/main/java/com/example/apptest1/MainActivity.java
+++ app/src/main/java/com/example/apptest1/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        AppCenter.start(getApplication(), "2b2fb485-d46e-4d96-8dca-6d12fb7226ef",
+                Analytics.class, Crashes.class);
     }
 }
